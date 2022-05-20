67331e79fc3ccc337964ec6e409d1b73282c0d39
JehanJay <jayawardana.ic@gmail.com>
2022-05-20 13:33:47 +0200

    bug fixes

--- listeners/tcp_transceiver.py
+++ listeners/tcp_transceiver.py
@@ -3,31 +3,12 @@ import sys
 
 from threading import Thread
 
-try: 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error as msg:
-    print("Could not create socket. Error Code: ", str(msg[0]), "Error: ", msg[1])
-    sys.exit(0)    
-ip = '0.0.0.0'
-port = 1141
-address = (ip, port)
-try:
-    s.bind(address)
-    print("[-] Socket bound to port " + str(port))
-except socket.error as msg:
-    print("Bind failed. Error code: {} Error: {}".format(str(msg[0]), msg[1]))
-    sys.exit()
-s.listen(10)
-print("[-] Started listening " + str(ip) + ":" + str(port))
 
 def listen_clients(conn):
     global connection
     connection = conn
-    sendReply = True    
     while True:
         data1 = conn.recv(4096)
-        #print(data1)
-        print(addr)
         data = data1.decode()
         print(data)
         if not data:
@@ -37,11 +18,33 @@ def listen_clients(conn):
     conn.close()
     sys.exit()
 
-while True:
-    conn, addr = s.accept()
-    print("connected")
-    t = Thread(target=listen_clients, args=(conn,))
-    t.start()
+
+def tcp_service():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as msg:
+        print("Could not create socket. Error Code: ", str(msg[0]), "Error: ", msg[1])
+        sys.exit(0)
+    ip = '0.0.0.0'
+    port = 1141
+    address = (ip, port)
+    try:
+        s.bind(address)
+        print("[-] Socket bound to port " + str(port))
+    except socket.error as msg:
+        print("Bind failed. Error code: {} Error: {}".format(str(msg[0]), msg[1]))
+        sys.exit()
+    s.listen(10)
+    print("[-] Started listening " + str(ip) + ":" + str(port))
+
+    while True:
+        conn, addr = s.accept()
+        print("connected")
+        t = Thread(target=listen_clients, args=(conn,))
+        t.start()
+
+    s.close()
 
 
-s.close()
+if __name__ == "__main__":
+    tcp_service()
